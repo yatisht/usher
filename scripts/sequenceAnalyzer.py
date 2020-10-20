d2820059d98ae90596f590ca8418be836b12a84f
Landen Gozashti <40596750+lgozasht@users.noreply.github.com>
2020-10-19 20:42:02 -0700

    Add files via upload

---
+++ scripts/sequenceAnalyzer.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+Created on Mon Apr 26 14:36:56 2018
+
+@author: landengozashti
+"""
+
+import sys
+
+class FastAreader :
+    ''' 
+    Define objects to read FastA files.
+    
+    instantiation: 
+    thisReader = FastAreader ('testTiny.fa')
+    usage:
+    for head, seq in thisReader.readFasta():
+        print (head,seq)
+    '''
+    def __init__ (self, fname=''):
+        '''contructor: saves attribute fname '''
+        self.fname = fname
+            
+    def doOpen (self):
+        ''' Handle file opens, allowing STDIN.'''
+        if self.fname is '':
+            return sys.stdin
+        else:
+            return open(self.fname)
+        
+    def readFasta (self):
+        ''' Read an entire FastA record and return the sequence header/sequence'''
+        header = ''
+        sequence = ''
+        
+        with self.doOpen() as fileH:
+            
+            header = ''
+            sequence = ''
+            
+            # skip to first fasta header
+            line = fileH.readline()
+            while not line.startswith('>') :
+                line = fileH.readline()
+            header = line[1:].rstrip()
+
+            for line in fileH:
+                if line.startswith('>'):
+                    yield header, sequence
+                    header = line[1:].rstrip()
+                    sequence = ''
+                else :
+                    sequence += ''.join(line.rstrip().split()) #.upper()
+
+        yield header, sequence
+
