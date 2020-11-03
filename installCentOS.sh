4aed3e141cc3244e20399be3aad06145ff4a64e7
Yatish Turakhia <yatisht@stanford.edu>
2020-11-03 10:01:39 -0800

    Update installCentOS.sh

--- installCentOS.sh
+++ installCentOS.sh
@@ -20,8 +20,8 @@ rm mafft-7.471-gcc_fc6.x86_64.rpm
 
 # install faToVcf
 rsync -aP rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/linux.x86_64/faToVcf .
-chmod 777 ./faToVcf
-mv ./faToVcf ./scripts/faToVcf
+chmod +x faToVcf
+mv faToVcf scripts/
 
 #install biopython
 pip3 install biopython
