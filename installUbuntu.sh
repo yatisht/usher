72e130950033de0c758f3746b68330807d82b4d1
Yatish Turakhia <yatisht@stanford.edu>
2020-11-20 14:33:26 -0800

    Update installUbuntu.sh

--- installUbuntu.sh
+++ installUbuntu.sh
@@ -1,12 +1,13 @@
 sudo apt update 
-sudo apt-get --yes install build-essential
-sudo apt-get --yes install wget
-sudo apt-get --yes install cmake
-sudo apt-get --yes install libboost-all-dev
-sudo apt-get --yes install libprotoc-dev libprotoc-dev protobuf-compiler
-sudo apt-get --yes install python python3-setuptools python3-pip 
-sudo apt-get install -y mafft
+sudo apt-get --yes install build-essential \
+ wget cmake  libboost-all-dev \
+ libprotoc-dev libprotoc-dev protobuf-compiler \
+ python python3-setuptools python3-pip mafft
+ 
+#install python library biopython
+pip3 install biopython
 
+#download and install TBB
 wget https://github.com/oneapi-src/oneTBB/archive/2019_U9.tar.gz 
 tar -xvzf 2019_U9.tar.gz
 mkdir -p build
@@ -19,6 +20,3 @@ cd ..
 rsync -aP rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/linux.x86_64/faToVcf .
 chmod +x faToVcf
 mv faToVcf scripts/
-
-#install biopython
-pip3 install biopython
