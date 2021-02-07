8dd7fc421185ce6fdafca588b8f9e15161d69460
Jakob <jmcbroome@gmail.com>
2021-02-07 11:53:50 -0800

    fixed compilation errors

--- src/matUtils.cpp
+++ src/matUtils.cpp
@@ -23,11 +23,11 @@ po::variables_map check_options(int argc, char** argv) {
          "Sample names to restrict. Use to perform masking") //this is now optional, as the Utils may be doing things other than masking. Should still perform the same given the same commands as previous.
         ("find-epps,e", po::bool_switch(),
         "Use to calculate and store the number of equally parsimonious placements for all nodes")
-        ("write-vcf,v", po::value<string>()->default_value(""),
+        ("write-vcf,v", po::value<std::string>()->default_value(""),
          "Output VCF file ")
         ("no-genotypes,n", po::bool_switch(),
         "Do not include sample genotype columns in VCF output. Used only with the vcf option")
-        ("write-tree,t", po::value<string>()->default_value(""),
+        ("write-tree,t", po::value<std::string>()->default_value(""),
          "Use to write a newick tree to the indicated file.")
         ("help,h", "Print help messages");
     
@@ -293,16 +293,7 @@ MAT::Tree findEPPs (MAT::Tree Tobj) {
 
 //CODE FOR VCF CONVERSION
 
-void make_vcf (MAT::Tree T, std::string vcf_filename, bool no_genotypes) {
-    auto vcf_filepath = outdir + "/" + vcf_filename;
-    FILE *vcf_file = fopen(vcf_filepath.c_str(), "w");
-    vector<Mutation_Annotated_Tree::Node*> dfs = T.depth_first_expansion();
-    write_vcf_header(vcf_file, dfs, !no_genotypes);
-    write_vcf_rows(vcf_file, T, dfs, !no_genotypes);
-    fclose(vcf_file);
-}
-
-void write_vcf_header(FILE *vcf_file, vector<Mutation_Annotated_Tree::Node*> &dfs,
+void write_vcf_header(FILE *vcf_file, std::vector<Mutation_Annotated_Tree::Node*> &dfs,
                       bool print_genotypes) {
     // Write minimal VCF header with sample names in same order that genotypes
     // will be printed out (DFS).
@@ -319,7 +310,7 @@ void write_vcf_header(FILE *vcf_file, vector<Mutation_Annotated_Tree::Node*> &df
     fputc('\n', vcf_file);
 }
 
-uint count_leaves(vector<Mutation_Annotated_Tree::Node*> &dfs) {
+uint count_leaves(std::vector<Mutation_Annotated_Tree::Node*> &dfs) {
     // Return the number of leaf nodes in dfs
     uint count = 0;
     for (auto node: dfs) {
@@ -340,9 +331,9 @@ int8_t *new_gt_array(int size, int8_t ref) {
 }
 
 uint r_add_genotypes(MAT::Node *node,
-                     unordered_map<string, vector<int8_t *>> &chrom_pos_genotypes, 
-                     unordered_map<string, vector<int8_t>> &chrom_pos_ref,
-                     uint leaf_count, uint leaf_ix, vector<struct MAT::Mutation *> &mut_stack) {
+                     std::unordered_map<std::string, std::vector<int8_t *>> &chrom_pos_genotypes, 
+                     std::unordered_map<std::string, std::vector<int8_t>> &chrom_pos_ref,
+                     uint leaf_count, uint leaf_ix, std::vector<struct MAT::Mutation *> &mut_stack) {
     // Traverse tree, adding leaf/sample genotypes for mutations annotated on path from root to node
     // to chrom_pos_genotypes (and reference allele to chrom_pos_ref).
     for (auto &mut: node->mutations) {
@@ -354,18 +345,18 @@ uint r_add_genotypes(MAT::Node *node,
     if (node->is_leaf()) {
         // Store genotypes in this leaf's column for all mutations on the path from root to leaf
         for (auto mut: mut_stack) {
-            string chrom = mut->chrom;
+            std::string chrom = mut->chrom;
             uint pos = (uint)mut->position;
             if (chrom.empty()) {
-              fprintf(stderr, "mut->chrom is empty string at node '%s', position %u\n",
+              fprintf(stderr, "mut->chrom is empty std::string at node '%s', position %u\n",
                       node->identifier.c_str(), pos);
             }
             if (chrom_pos_genotypes.find(chrom) == chrom_pos_genotypes.end()) {
                 // First variant on chrom: initialize a vector mapping position to genotype.
                 // Assume a genome size similar to SARS-CoV-2, resize if necessary.
                 uint initSize = 30000;
-                chrom_pos_genotypes[chrom] = vector<int8_t *>(initSize);
-                chrom_pos_ref[chrom] = vector<int8_t>(initSize);
+                chrom_pos_genotypes[chrom] = std::vector<int8_t *>(initSize);
+                chrom_pos_ref[chrom] = std::vector<int8_t>(initSize);
             }
             if (pos >= chrom_pos_genotypes[chrom].size()) {
                 // chrom has larger positions than we assumed; allocate a larger vector.
@@ -394,9 +385,9 @@ uint r_add_genotypes(MAT::Node *node,
     return leaf_ix;
 }
 
-unordered_map<int8_t, uint>count_alleles(int8_t *gt_array, uint gtCount)  {
+std::unordered_map<int8_t, uint>count_alleles(int8_t *gt_array, uint gtCount)  {
     // Tally up the count of each allele (both ref and alts) from sample genotypes.
-    unordered_map<int8_t, uint> allele_counts;
+    std::unordered_map<int8_t, uint> allele_counts;
     for (uint i = 0;  i < gtCount;  i++) {
         int8_t allele = gt_array[i];
         if (allele_counts.find(allele) == allele_counts.end()) {
@@ -408,42 +399,42 @@ unordered_map<int8_t, uint>count_alleles(int8_t *gt_array, uint gtCount)  {
     return allele_counts;
 }
 
-bool cmp_allele_count_desc(const pair<int8_t, uint>& a, const pair<int8_t, uint>& b) {
+bool cmp_allele_count_desc(const std::pair<int8_t, uint>& a, const std::pair<int8_t, uint>& b) {
     // Compare counts of two alleles, for sorting in descending order.
     return a.second > b.second;
 }
 
-map<int8_t, uint>make_alts(unordered_map<int8_t, uint> &allele_counts, int8_t ref) {
+std::map<int8_t, uint>make_alts(std::unordered_map<int8_t, uint> &allele_counts, int8_t ref) {
     // Map alternate alleles, ordered by count (highest first), to counts.
-    vector<pair<int8_t, uint>> pairs;
+    std::vector<std::pair<int8_t, uint>> pairs;
     for (auto &itr : allele_counts) {
         if (itr.first != ref) {
             pairs.push_back(itr);
         }
     }
-    sort(pairs.begin(), pairs.end(), cmp_allele_count_desc);
-    map<int8_t, uint> alts;
+    std::sort(pairs.begin(), pairs.end(), cmp_allele_count_desc);
+    std::map<int8_t, uint> alts;
     for (auto &itr : pairs) {
       alts.insert(itr);
     }
     return alts;
 }
 
-string make_id(int8_t ref, uint pos, map<int8_t, uint> &alts) {
-    // Return a C string comma-sep list of the form <ref><pos><alt1>[,<ref><pos><alt2>[,...]].
-    string id;
+std::string make_id(int8_t ref, uint pos, std::map<int8_t, uint> &alts) {
+    // Return a C std::string comma-sep list of the form <ref><pos><alt1>[,<ref><pos><alt2>[,...]].
+    std::string id;
     for (auto &itr : alts) {
         if (! id.empty()) {
             id += ",";
         }
-        id += MAT::get_nuc(ref) + to_string(pos) + MAT::get_nuc(itr.first);
+        id += MAT::get_nuc(ref) + std::to_string(pos) + MAT::get_nuc(itr.first);
     }
     return id;
 }
 
-string make_alt_str(map<int8_t, uint> &alts) {
-    // Return a C string comma-sep list of alternate alleles.
-    string alt_str;
+std::string make_alt_str(std::map<int8_t, uint> &alts) {
+    // Return a C std::string comma-sep list of alternate alleles.
+    std::string alt_str;
     for (auto &itr : alts) {
         if (! alt_str.empty()) {
           alt_str += ",";
@@ -453,21 +444,21 @@ string make_alt_str(map<int8_t, uint> &alts) {
     return alt_str;
 }
 
-string make_info(map<int8_t, uint> &alts, uint leaf_count) {
-    // Return a C string VCF INFO value with AC (comma-sep list of alternate allele counts)
+std::string make_info(std::map<int8_t, uint> &alts, uint leaf_count) {
+    // Return a C std::string VCF INFO value with AC (comma-sep list of alternate allele counts)
     // and AN (total genotype count).
-    string alt_count_str;
+    std::string alt_count_str;
     for (auto &itr : alts) {
         if (! alt_count_str.empty()) {
             alt_count_str += ",";
         }
-        alt_count_str += to_string(itr.second);
+        alt_count_str += std::to_string(itr.second);
     }
-    string info = "AC=" + alt_count_str + ";AN=" + to_string(leaf_count);
+    std::string info = "AC=" + alt_count_str + ";AN=" + std::to_string(leaf_count);
     return info;
 }
 
-int *make_allele_codes(int8_t ref, map<int8_t, uint> &alts) {
+int *make_allele_codes(int8_t ref, std::map<int8_t, uint> &alts) {
     // Return an array that maps binary-encoded nucleotide to VCF genotype encoding:
     // 0 for reference allele, 1 for first alternate allele, and so on.
     int *al_codes = new int[256];
@@ -482,28 +473,28 @@ int *make_allele_codes(int8_t ref, map<int8_t, uint> &alts) {
     return al_codes;
 }
 
-void write_vcf_rows(FILE *vcf_file, MAT::Tree T, vector<MAT::Node*> &dfs, bool print_genotypes) {
+void write_vcf_rows(FILE *vcf_file, MAT::Tree T, std::vector<MAT::Node*> &dfs, bool print_genotypes) {
     // Fill in a matrix of genomic positions and sample genotypes in the same order as the
     // sample names in the header, compute allele counts, and output VCF rows.
     uint leaf_count = count_leaves(dfs);
     // The int8_t here is mutation_annotated_tree.hpp's binary encoding of IUPAC nucleotide bases.
-    unordered_map<string, vector<int8_t *>> chrom_pos_genotypes;
-    unordered_map<string, vector<int8_t>> chrom_pos_ref;
-    vector<struct MAT::Mutation *> mut_stack;
+    std::unordered_map<std::string, std::vector<int8_t *>> chrom_pos_genotypes;
+    std::unordered_map<std::string, std::vector<int8_t>> chrom_pos_ref;
+    std::vector<struct MAT::Mutation *> mut_stack;
     r_add_genotypes(T.root, chrom_pos_genotypes, chrom_pos_ref, leaf_count, 0, mut_stack);
     // Write row of VCF for each variant in chrom_pos_genotypes[chrom]
     for (auto itr = chrom_pos_genotypes.begin();  itr != chrom_pos_genotypes.end();  ++itr) {
-        string chrom = itr->first;
-        vector<int8_t *> pos_genotypes = itr->second;
+        std::string chrom = itr->first;
+        std::vector<int8_t *> pos_genotypes = itr->second;
         for (uint pos = 0;  pos < pos_genotypes.size();  pos++) {
             int8_t *gt_array = pos_genotypes[pos];
             if (gt_array) {
               int8_t ref = chrom_pos_ref[chrom][pos];
-              unordered_map<int8_t, uint>allele_counts = count_alleles(gt_array, leaf_count);
-              map<int8_t, uint>alts = make_alts(allele_counts, ref);
-              string id = make_id(ref, pos, alts);
-              string alt_str = make_alt_str(alts);
-              string info = make_info(alts, leaf_count);
+              std::unordered_map<int8_t, uint>allele_counts = count_alleles(gt_array, leaf_count);
+              std::map<int8_t, uint>alts = make_alts(allele_counts, ref);
+              std::string id = make_id(ref, pos, alts);
+              std::string alt_str = make_alt_str(alts);
+              std::string info = make_info(alts, leaf_count);
               fprintf(vcf_file, "%s\t%d\t%s\t%c\t%s\t.\t.\t%s",
                       chrom.c_str(), pos, id .c_str(), MAT::get_nuc(ref), alt_str.c_str(),
                       info.c_str());
@@ -521,6 +512,15 @@ void write_vcf_rows(FILE *vcf_file, MAT::Tree T, vector<MAT::Node*> &dfs, bool p
     }
 }
 
+void make_vcf (MAT::Tree T, std::string vcf_filename, bool no_genotypes) {
+    auto vcf_filepath = "./" + vcf_filename; //taking away a touch of functionality for simplicity. just write it to this directory.
+    FILE *vcf_file = fopen(vcf_filepath.c_str(), "w");
+    std::vector<Mutation_Annotated_Tree::Node*> dfs = T.depth_first_expansion();
+    write_vcf_header(vcf_file, dfs, !no_genotypes);
+    write_vcf_rows(vcf_file, T, dfs, !no_genotypes);
+    fclose(vcf_file);
+}
+
 int main(int argc, char** argv) {
 
     // Command line options
@@ -528,11 +528,11 @@ int main(int argc, char** argv) {
     std::string input_mat_filename = vm["input-mat"].as<std::string>();
     std::string output_mat_filename = vm["output-mat"].as<std::string>();
     std::string samples_filename = vm["restricted-samples"].as<std::string>();
-    std::string tree_filename = vm["write-tree"].as<string>();
-    std::string vcf_filename = vm["write-vcf"].as<string>();
+    std::string tree_filename = vm["write-tree"].as<std::string>();
+    std::string vcf_filename = vm["write-vcf"].as<std::string>();
 
     bool fepps = vm["find-epps"].as<bool>();
-    bool no_genotypes = vm['no-genotypes'].as<bool>();
+    bool no_genotypes = vm["no-genotypes"].as<bool>();
 
     // Load input MAT and uncondense tree
     MAT::Tree T = MAT::load_mutation_annotated_tree(input_mat_filename);
@@ -555,7 +555,7 @@ int main(int argc, char** argv) {
     }
     //if a newick tree filename was given, write a tree to it
     if (tree_filename != "") {
-        auto tree_filepath = outdir + "/" + tree_filename;
+        auto tree_filepath = "./" + tree_filename; //for simplicity, write it to this directory
         FILE *tree_file = fopen(tree_filepath.c_str(), "w");
         fprintf(tree_file, "%s\n",
             MAT::get_newick_string(T, true, true, true).c_str());
