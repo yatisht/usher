63c4cf7febf62e6503771d06fc39a45ff881a805
Cheng Ye <chye@ucsd.edu>
2021-02-20 20:38:53 +0000

    update test script

---
+++ collect_stat.pl
@@ -0,0 +1,39 @@
+use warnings;
+use strict;
+sub get_stat{
+	my ($path)=@_;
+	my ($radius, $test_case)=$path=~/.*r(\d+)\/(.*)\/log/;
+	open(my $FH,"<",$path);
+	my $before;
+	my $after;
+	my $n_nodes;
+	my $realtime;
+	my $usertime;
+	while(<$FH>){
+		if($_=~/Before refinement: (\d+)/){
+			$before=$1;
+		}
+		if($_=~/After refinement: (\d+)/){
+			$after=$1;
+		}
+		if($_=~/real	(.*)/){
+			$realtime=$1;
+		}
+		if($_=~/user	(.*)/){
+			$usertime=$1;
+		}
+		if($_=~/Current tree size \(#nodes\): (\d*)/){
+			$n_nodes=$1;
+		}
+	}
+	if(defined $realtime){
+		print("$radius,$test_case,$n_nodes,$before,$after,$realtime,$usertime\n");
+	}
+}
+
+my @logs=glob("testout/r*/*/log");
+print("Radius,Test Case,Numer of Nodes,Before,After,Real Time,CPU Time\n");
+foreach(@logs){
+	get_stat($_);
+}
+
--- test_refine.sh
+++ test_refine.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
-mkdir -p testout
-rm -f testout/*
-build/usher --refine $3 -i $1 -v $2 -o testout/refined.pb
-build/usher -i $1 -v $2 -o testout/not-refined.pb
-mkfifo testout/refined.vcf
-mkfifo testout/not-refined.vcf
-build/matUtils convert -i testout/refined.pb -v testout/refined.vcf&
-perl vcfNormalizeGt.pl testout/refined.vcf >testout/refined.normalized.vcf
-build/matUtils convert -i testout/not-refined.pb -v testout/not-refined.vcf&
-perl vcfNormalizeGt.pl testout/not-refined.vcf >testout/not-refined.normalized.vcf
-diff -s testout/refined.normalized.vcf testout/not-refined.normalized.vcf
-wc testout/refined.normalized.vcf
-wc testout/not-refined.normalized.vcf
+test_path=$1
+radius=$2
+test_case_name=$(basename $1)
+outpath="testout/r$radius/$test_case_name/"
+pb=$(realpath $test_path/*.pb)
+vcf=$(realpath $test_path/missing*.vcf.gz)
+
+mkdir -p $outpath
+rm -f $outpath/*
+{
+time build/usher --refine $radius -i $pb -v $vcf -o $outpath/refined.pb
+} &> $outpath/log
+build/usher -i $pb -v $vcf -o $outpath/not-refined.pb &> /dev/null
+{
+build/matUtils convert -i $outpath/refined.pb -v $outpath/refined.vcf
+perl vcfNormalizeGt.pl $outpath/refined.vcf >$outpath/refined.normalized.vcf
+}&
+{
+build/matUtils convert -i $outpath/not-refined.pb -v $outpath/not-refined.vcf
+perl vcfNormalizeGt.pl $outpath/not-refined.vcf >$outpath/not-refined.normalized.vcf
+}&
+wait
+{
+diff -s $outpath/refined.normalized.vcf $outpath/not-refined.normalized.vcf
+wc $outpath/refined.normalized.vcf
+wc $outpath/not-refined.normalized.vcf
+} &> $outpath/diff-out
