175a4fb189ba8fce7c662e891c48313a3b772246
Jakob <jmcbroome@gmail.com>
2021-02-23 16:23:07 -0800

    Reworked prune code to use dynamic switch based on input sample set size; changed input parameters to separate mode and sample input

--- src/matUtils/prune.cpp
+++ src/matUtils/prune.cpp
@@ -11,10 +11,10 @@ po::variables_map parse_prune_command(po::parsed_options parsed) {
          "Input mutation-annotated tree file [REQUIRED]")
         ("output-mat,o", po::value<std::string>()->required(),
          "Output mutation-annotated tree file [REQUIRED]")
-        ("prune-samples,p", po::value<std::string>()->default_value(""),
-         "File containing names of samples (one per line) to be pruned from the input MAT ")
-        ("prune-all-but-samples,P", po::value<std::string>()->default_value(""),
-         "File containing names of samples (one per line) to be maintained (remaining are pruned) from the input MAT ")
+        ("samples,s", po::value<std::string>()->required(),
+        "File containing names of samples (one per line).")
+        ("prune,p", po::bool_switch(),
+        "Use to remove the indicated samples from the tree. Default is to return a subtree MAT containing only the indicated samples.")
         ("threads,T", po::value<uint32_t>()->default_value(num_cores), num_threads_message.c_str())
         ("help,h", "Print help messages");
     // Collect all the unrecognized options from the first pass. This will include the
@@ -40,49 +40,6 @@ po::variables_map parse_prune_command(po::parsed_options parsed) {
     return vm;
 }
 
-void prune_main(po::parsed_options parsed) {
-    po::variables_map vm = parse_prune_command(parsed);
-    std::string input_mat_filename = vm["input-mat"].as<std::string>();
-    std::string output_mat_filename = vm["output-mat"].as<std::string>();
-    std::string prune_samples_filename = vm["prune-samples"].as<std::string>();
-    std::string prune_all_but_samples_filename = vm["prune-all-but-samples"].as<std::string>();
-    uint32_t num_threads = vm["threads"].as<uint32_t>();
-
-    if ((prune_samples_filename == "") && (prune_all_but_samples_filename == "")) {
-        fprintf(stderr, "ERROR: At least one of --prune-samples and --prune-all-but-samples must be set!\n");
-        exit(1);
-    }
-    if ((prune_samples_filename != "") && (prune_all_but_samples_filename != "")) {
-        fprintf(stderr, "ERROR: Both --prune-samples and --prune-all-but-samples cannot be set!\n");
-        exit(1);
-    }
-
-    tbb::task_scheduler_init init(num_threads);
-
-    timer.Start();
-    fprintf(stderr, "Loading input MAT file %s.\n", input_mat_filename.c_str()); 
-    // Load input MAT and uncondense tree
-    MAT::Tree T = MAT::load_mutation_annotated_tree(input_mat_filename);
-    T.uncondense_leaves();
-    fprintf(stderr, "Completed in %ld msec \n\n", timer.Stop());
-
-    MAT::Tree subtree;
-    if (prune_samples_filename != "") {
-        subtree = prune_leaves(T, prune_samples_filename);
-    }
-    
-    if (prune_all_but_samples_filename != "") {
-        subtree = prune_all_but_leaves(T, prune_all_but_samples_filename);
-    }
-    
-    timer.Start();
-    fprintf(stderr, "Saving output MAT file %s.\n", output_mat_filename.c_str()); 
-    // Save output MAT
-    subtree.condense_leaves();
-    MAT::save_mutation_annotated_tree(subtree, output_mat_filename);
-    fprintf(stderr, "Completed in %ld msec \n\n", timer.Stop());
-}
-
 void read_sample_names (std::string sample_filename, std::vector<std::string>& sample_names) {
     std::ifstream infile(sample_filename);
     if (!infile) {
@@ -105,13 +62,52 @@ void read_sample_names (std::string sample_filename, std::vector<std::string>& s
     infile.close();
 }
 
-MAT::Tree prune_leaves (const MAT::Tree& T, std::string sample_filename) {
-    std::vector<std::string> sample_names;
+void prune_main(po::parsed_options parsed) {
+    po::variables_map vm = parse_prune_command(parsed);
+    std::string input_mat_filename = vm["input-mat"].as<std::string>();
+    std::string output_mat_filename = vm["output-mat"].as<std::string>();
+    std::string sample_filename = vm["samples"].as<std::string>();
+    bool prune = vm["prune"].as<bool>();
+    uint32_t num_threads = vm["threads"].as<uint32_t>();
 
+    tbb::task_scheduler_init init(num_threads);
+
+    timer.Start();
+    fprintf(stderr, "Loading input MAT file %s.\n", input_mat_filename.c_str()); 
+    // Load input MAT and uncondense tree
+    MAT::Tree T = MAT::load_mutation_annotated_tree(input_mat_filename);
+    T.uncondense_leaves();
+    fprintf(stderr, "Completed in %ld msec \n\n", timer.Stop());
+
+    std::vector<std::string> sample_names;
     timer.Start();
     fprintf(stderr, "Loading sample names from %s.\n", sample_filename.c_str()); 
     read_sample_names(sample_filename, sample_names);
     fprintf(stderr, "Completed in %ld msec \n\n", timer.Stop());
+
+    MAT::Tree subtree;
+    if (prune) {
+        subtree = prune_leaves(T, sample_names);
+    } else if (sample_names.size() < 10000) {
+        //for retaining only a subtree, get_subtree is the most effective method
+        //for subtress up to about 10 thousand samples in size; after that, pruning
+        //all other nodes becomes more efficient, because get_subtree scales with 
+        //the size of the input sample set, while prune takes a similar time for 
+        //any sample set size, while scaling on total tree size
+        subtree = get_sample_subtree(T, sample_names);
+    } else {
+        subtree = get_sample_prune(T, sample_names);
+    }
+    
+    timer.Start();
+    fprintf(stderr, "Saving output MAT file %s.\n", output_mat_filename.c_str()); 
+    // Save output MAT
+    subtree.condense_leaves();
+    MAT::save_mutation_annotated_tree(subtree, output_mat_filename);
+    fprintf(stderr, "Completed in %ld msec \n\n", timer.Stop());
+}
+
+MAT::Tree prune_leaves (const MAT::Tree& T, std::vector<std::string> sample_names) {
 
     timer.Start();
     fprintf(stderr, "Pruning %zu samples.\n", sample_names.size());
@@ -129,19 +125,36 @@ MAT::Tree prune_leaves (const MAT::Tree& T, std::string sample_filename) {
     return subtree;
 }
 
-MAT::Tree prune_all_but_leaves (const MAT::Tree& T, std::string sample_filename) {
-    std::vector<std::string> sample_names;
+MAT::Tree get_sample_subtree (const MAT::Tree& T, std::vector<std::string> sample_names) {
 
     timer.Start();
-    fprintf(stderr, "Loading sample names from %s.\n", sample_filename.c_str()); 
-    read_sample_names(sample_filename, sample_names);
-    fprintf(stderr, "Completed in %ld msec \n\n", timer.Stop());
-
-    timer.Start();
-    fprintf(stderr, "Pruning all but %zu samples.\n", sample_names.size());
+    fprintf(stderr, "Extracting subtree of %zu samples.\n", sample_names.size());
     auto subtree = MAT::get_subtree(T, sample_names);
     fprintf(stderr, "Completed in %ld msec \n\n", timer.Stop());
 
     return subtree;
 }
 
+MAT::Tree get_sample_prune (const MAT::Tree& T, std::vector<std::string> sample_names) {
+    
+    timer.Start();
+    fprintf(stderr, "Large sample input; building subtree by pruning all but %zu samples.\n", sample_names.size());
+    //its going to be much quicker to check membership in a set then a vector O(logN)
+    //which is important when we're looking at large sample inputs
+    //so convert the sample names vector to an unordered set
+    std::unordered_set<std::string> setnames(sample_names.begin(),sample_names.end());
+
+    auto subtree = MAT::get_tree_copy(T);
+    auto dfs = T.depth_first_expansion();
+    for (auto s: dfs) {
+        //only call the remover on leaf nodes (can't be deleting the root...)
+        if (s->is_leaf()) {
+            //if the node is NOT in the set, remove it
+            if (setnames.find(s->identifier) == setnames.end()) {
+                subtree.remove_node(s->identifier, true);
+            }
+        }
+    }    
+    fprintf(stderr, "Completed in %ld msec \n\n", timer.Stop());
+    return subtree;
+}
