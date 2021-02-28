bc95d682bdf771f4294a2700ef43a79ff34a1fbc
Jakob <jmcbroome@gmail.com>
2021-02-28 12:15:26 -0800

    Refactoring uncertainty to make it includable

--- src/matUtils/uncertainty.hpp
+++ src/matUtils/uncertainty.hpp
@@ -3,6 +3,7 @@
 std::vector<MAT::Node*> get_common_nodes (std::vector<std::vector<MAT::Node*>> nodepaths);
 std::vector<float> get_all_distances(MAT::Node* target, std::vector<std::vector<MAT::Node*>> paths);
 size_t get_neighborhood_size(std::vector<MAT::Node*> nodes, MAT::Tree* T);
-void findEPPs (MAT::Tree Tobj, std::string sample_file, std::string fepps, std::string fneigh);
+void findEPPs (MAT::Tree* T, MAT::Node* node, bool get_nsize, size_t* nbest, size_t* nsize);
+void findEPPs_wrapper (MAT::Tree Tobj, std::string sample_file, std::string fepps, std::string fneigh);
 po::variables_map parse_uncertainty_command(po::parsed_options parsed);
 void uncertainty_main(po::parsed_options parsed);
