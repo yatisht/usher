60ee0628349c63ac5a8ab7621fdb665675dca640
Jakob <jmcbroome@gmail.com>
2021-02-28 14:43:21 -0800

    adding select module; contains includable functions for getting sets of samples

---
+++ src/matUtils/select.cpp
@@ -0,0 +1,102 @@
+#include "select.hpp"
+#include "uncertainty.hpp"
+
+/*
+Functions in this module take a variety of arguments, usually including a MAT
+and return a set of samples as a std::vector<std::string>
+*/
+
+std::vector<std::string> read_sample_names (std::string sample_filename) {
+    std::vector<std::string> sample_names;
+    std::ifstream infile(sample_filename);
+    if (!infile) {
+        fprintf(stderr, "ERROR: Could not open the file: %s!\n", sample_filename.c_str());
+        exit(1);
+    }    
+    std::string line;
+    bool warned = false;
+    while (std::getline(infile, line)) {
+        std::vector<std::string> words;
+        MAT::string_split(line, words);
+        if (words.size() != 1 && !warned) {
+            fprintf(stderr, "WARNING: Input file %s contains excess columns; ignoring\n", sample_filename.c_str());
+            warned = true;
+        }
+        //remove carriage returns from the input to handle windows os 
+        sname = words[0];
+        if (sname[sname.size()-1] == '\r') {
+            sname = sname.substr(0,sname.size()-1);
+        }
+        sample_names.push_back(std::move(sname));
+    }
+    infile.close();
+    return sample_names;
+}
+
+std::vector<std::string> get_clade_samples (const MAT::Tree& T, std::string clade_name) {
+    //fetch the set of sample names associated with a clade name to pass downstream in lieu of reading in a sample file.
+
+    std::vector<std::string> csamples;
+    auto dfs = T.depth_first_expansion();
+    for (auto s: dfs) {
+        if (s->is_leaf()) {
+            //check if the clade name is listed anywhere in the annotation vector.
+            if (std::find(s->clade_annotations.begin(), s->clade_annotations.end(), clade_name) != s->clade_annotations.end()) {
+                csamples.push_back(s->identifier);
+            }
+        }
+    }
+    return csamples;
+}
+
+std::vector<std::string> get_mutation_samples (const MAT::Tree& T, std::string mutation_id) {
+    //fetch the set of sample names which contain a given mutation.
+    //this is a naive implementation parallel to describe::mutation_paths
+    std::vector<std::string> good_samples;
+
+    for (auto node: T->get_leaves()) {
+        bool assigned = false;
+        //first, check if this specific sample has the mutation 
+        for (auto m: node->mutations) {
+            if (m.get_string() == mutation_id) {
+                good_samples.push_back(node->identifier);
+                assigned = true;
+                break;
+            }
+        } 
+        if (!assigned) {
+            std::vector<MAT::Node*> path = T->rsearch(node);
+            //for every ancestor up to the root, check if they have the mutation
+            //if they do, break, save the name, move to the next sample
+            for (auto anc_node: path) {
+                if (!assigned) {
+                    for (auto m: anc_node->mutations) {
+                        good_samples.push_back(node->identifier);
+                        assigned = true;
+                        break;                
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    return good_samples;
+}
+
+std::vector<std::string> get_samples_epps (const MAT::Tree& T, size_t max_epps) {
+    //calculate uncertainty for all samples in the tree
+    //and return the set of samples which have EPPs less than max_epps
+    //default filter value is 1, which 85% of samples have
+    std::vector<std::string> good_samples;
+    auto dfs = T->depth_first_expansion();
+    for (auto n: dfs) {
+        size_t nb;
+        size_t ns;
+        findEPPs(T, n, false, &nb, &ns);
+        if (nb <= max_epps) {
+            good_samples.push_back(n->identifier);
+        }
+    }
+    return good_samples;
+}
