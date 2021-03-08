15485736d2affbaf4d039146d1122aa24650fb80
Landen Gozashti <40596750+lgozasht@users.noreply.github.com>
2021-03-08 15:41:57 -0800

    Update installMacOS.sh

--- installMacOS.sh
+++ installMacOS.sh
@@ -14,7 +14,6 @@ cd ..
 # install faToVcf
 rsync -aP rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/macOSX.x86_64/faToVcf .
 chmod +x faToVcf
-mv faToVcf scripts/
 
 #install biopython
 pip3 install biopython
