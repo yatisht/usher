bba78e57eca839462d312146602a92280f960ca8
Jakob McBroome <jmcbroome@gmail.com>
2021-05-12 17:51:23 -0700

    remove deprecated functions and change default display behavior

--- src/matUtils/convert.hpp
+++ src/matUtils/convert.hpp
@@ -2,5 +2,4 @@
 
 void make_vcf (MAT::Tree T, std::string vcf_filename, bool no_genotypes);
 void write_json_from_mat(MAT::Tree* T, std::string output_filename, std::map<std::string,std::map<std::string,std::string>> catmeta);
-void make_json (MAT::Tree T, std::string json_filename);
 MAT::Tree load_mat_from_json(std::string json_filename);
