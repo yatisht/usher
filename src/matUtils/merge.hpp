2361bcbf2e734a974f255d288b664b44d7817f8e
dtorvi <83394090+dtorvi@users.noreply.github.com>
2021-07-07 11:25:18 -0700

    Update merge.hpp

--- src/matUtils/merge.hpp
+++ src/matUtils/merge.hpp
@@ -1,4 +1,5 @@
 #include "common.hpp"
 po::variables_map parse_summary_command(po::parsed_options parsed);
 bool consistent(MAT::Tree T, MAT::Tree B);
+bool chelper(MAT::Node* a, MAT::Node* b);
 void merge_main(po::parsed_options parsed);
