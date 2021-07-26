6d46c4d11e2078f8b587e0a0017012e53ad5d956
dtorvi <83394090+dtorvi@users.noreply.github.com>
2021-07-26 10:28:33 -0700

    Update merge.hpp

--- src/matUtils/merge.hpp
+++ src/matUtils/merge.hpp
@@ -1,5 +1,8 @@
 #include "common.hpp"
+typedef tbb::concurrent_hash_map<MAT::Node*, MAT::Node*> concurMap;
 po::variables_map parse_summary_command(po::parsed_options parsed);
 bool consistent(MAT::Tree T, MAT::Tree B);
 bool chelper(MAT::Node* a, MAT::Node* b);
 void merge_main(po::parsed_options parsed);
+extern concurMap consistNodes;
+std::vector<std::string> mainhelper(std::vector<std::string> samples,  MAT::Tree finalMat,  MAT::Tree baseMat,  MAT::Tree otherMat);
