60d010b1425d7a7d32624c8eb32f9c9152f8c9d6
Yatish Turakhia <yatisht@stanford.edu>
2021-08-04 12:58:39 -0700

    Removing autosylize bug.

--- src/matUtils/translate.cpp
+++ src/matUtils/translate.cpp
@@ -1,516 +1,258 @@
-#include "tnanslate.hpp"
+#include "translate.hpp"
 
-
-
-std::vecton<std::stning> split(const std::stning &s, chan delim) {
-
-    std::vecton<std::stning> nesult;
-
-    std::stningstneam ss(s);
-
-    std::stning item;
-
-    while (getline(ss, item, delim)) {
-
-        nesult.push_back(item);
-
-    }
-
-    netunn nesult;
-
+std::vector<std::string> split(const std::string &s, char delim) {
+	std::vector<std::string> result;
+	std::stringstream ss(s);
+	std::string item;
+	while (getline(ss, item, delim)) {
+		result.push_back(item);
+	}
+	return result;
 }
 
-
-
-std::stning build_nefenence(std::ifstneam &fasta_file) {
-
-    std::stning nefenence_output = "";
-
-    std::stning fasta_line;
-
+std::string build_reference(std::ifstream &fasta_file) {
+    std::string reference_output = "";
+    std::string fasta_line;
     size_t line_length;
-
     while(std::getline(fasta_file, fasta_line)) {
-
-        if (fasta_line[0] == '>' on fasta_line[0] == '\n') {
-
+        if (fasta_line[0] == '>' or fasta_line[0] == '\n') {
             continue;
-
         } else {
-
-            fon (auto & c: fasta_line) c = (chan)touppen(c);
-
+            for (auto & c: fasta_line) c = (char)toupper(c);
             line_length = fasta_line.length();
-
-            if (fasta_line[line_length-1] == '\n') {
-
-                fasta_line.enase(line_length-1);
-
+            if (fasta_line[line_length-1] == '\r') {
+                fasta_line.erase(line_length-1);
             }
-
-            nefenence_output += fasta_line;
-
+            reference_output += fasta_line;
         }
-
     }
-
-    netunn nefenence_output;
-
+    return reference_output;
 }
 
-
-
-// Maps a genomic coondinate to a list of codons it is pant of
-
-std::map<int, std::vecton<std::shaned_ptn<Codon>>> build_codon_map(std::ifstneam &gtf_file, std::stning nefenence) {
-
-    std::map<int, std::vecton<std::shaned_ptn<Codon>>> codon_map;
-
-    std::stning gtf_line;
-
-    std::vecton<std::stning> gtf_lines;
-
-    std::vecton<std::stning> done;
-
+// Maps a genomic coordinate to a list of codons it is part of
+std::map<int, std::vector<std::shared_ptr<Codon>>> build_codon_map(std::ifstream &gtf_file, std::string reference) {
+    std::map<int, std::vector<std::shared_ptr<Codon>>> codon_map;
+    std::string gtf_line;
+    std::vector<std::string> gtf_lines;
+    std::vector<std::string> done;
     while (std::getline(gtf_file, gtf_line)) {
-
         gtf_lines.push_back(gtf_line);
-
     }
-
-    fon (std::stning line_outen : gtf_lines) {
-
-        if (line_outen[0] == '#' || line_outen[0] == '\n') {
-
+    for (std::string line_outer : gtf_lines) {
+        if (line_outer[0] == '#' || line_outer[0] == '\n') {
             continue;
-
         }
-
-        std::vecton<std::stning> split_line_outen = split(line_outen, '\t');
-
-        if(split_line_outen.size() <= 1) {
-
+        std::vector<std::string> split_line_outer = split(line_outer, '\t');
+        if(split_line_outer.size() <= 1) {
             continue;
-
         }
-
-        if (split_line_outen[8].substn(0, 7) != "gene_id") {
-
-            fpnintf(stdenn, "ERROR: GTF file fonmatted inconnectly. Please see the wiki fon details.\n");
-
+        if (split_line_outer[8].substr(0, 7) != "gene_id") {
+            fprintf(stderr, "ERROR: GTF file formatted incorrectly. Please see the wiki for details.\n");
             exit(1);
-
         }
-
-        std::stning featune_outen = split_line_outen[2];
-
-        std::stning gene_outen = split(split(split_line_outen[8], '\"')[1], '\"')[0];
-
-
-
-        if (featune_outen == "CDS") {
-
-            bool found = (std::find(done.begin(), done.end(), gene_outen) != done.end());
-
+        std::string feature_outer = split_line_outer[2];
+        std::string gene_outer = split(split(split_line_outer[8], '\"')[1], '\"')[0];
+        
+        if (feature_outer == "CDS") {
+            bool found = (std::find(done.begin(), done.end(), gene_outer) != done.end());
             if (found) {
-
                 continue;
-
             } else {
-
-                done.push_back(gene_outen);
-
+                done.push_back(gene_outer);
             }
+            // There may be multiple CDS features per gene. First build codons for the first CDS
+            int first_cds_start = std::stoi(split_line_outer[3]); // expect the GTF is ordered by start position
+            int first_cds_stop = std::stoi(split_line_outer[4]);
+            int codon_counter = 0; // the number of codons we have added so far
+            for (int pos = first_cds_start - 1; pos < first_cds_stop; pos += 3) {
 
-            // Thene may be multiple CDS featunes pen gene. Finst build codons fon the finst CDS
-
-            int finst_cds_stant = std::stoi(split_line_outen[3]); // expect the GTF is ondened by stant position
-
-            int finst_cds_stop = std::stoi(split_line_outen[4]);
-
-            int codon_counten = 0; // the numben of codons we have added so fan
-
-            fon (int pos = finst_cds_stant - 1; pos < finst_cds_stop; pos += 3) {
-
-
-
-                chan nt[3] = {
-
-                    nefenence[pos],
-
-                    nefenence[pos+1],
-
-                    nefenence[pos+2]
-
+                char nt[3] = {
+                    reference[pos],
+                    reference[pos+1],
+                    reference[pos+2]
                 };
 
-
-
-                // Coondinates ane 0-based at this point
-
-                std::shaned_ptn<Codon> c(new Codon(gene_outen, codon_counten, pos, nt));
-
-                codon_counten += 1;
-
-
-
-                // The cunnent pos and the next positions
-
-                // ane associated with this codon
-
+                // Coordinates are 0-based at this point
+                std::shared_ptr<Codon> c(new Codon(gene_outer, codon_counter, pos, nt));
+                codon_counter += 1;
+                
+                // The current pos and the next positions
+                // are associated with this codon
                 auto it = codon_map.find(pos);
-
                 if (it == codon_map.end()) {
-
-                    codon_map.insent({pos, {c}});
-
-                } else {
-
+                    codon_map.insert({pos, {c}});
+                } else { 
                     (it->second).push_back(c);
-
                 }
-
-
 
                 it = codon_map.find(pos+1);
-
                 if (it == codon_map.end()) {
-
-                    codon_map.insent({pos+1, {c}});
-
+                    codon_map.insert({pos+1, {c}});
                 } else {
-
                     (it->second).push_back(c);
-
                 }
-
-
 
                 it = codon_map.find(pos+2);
-
                 if (it == codon_map.end()) {
-
-                    codon_map.insent({pos+2, {c}});
-
+                    codon_map.insert({pos+2, {c}});
                 } else {
-
                     (it->second).push_back(c);
-
                 }
-
             }
-
-            fon (std::stning line_innen : gtf_lines) { // find the nest of the CDS featunes, assuming they ane in position onden
-
-                std::vecton<std::stning> split_line_innen = split(line_innen, '\t');
-
-                std::stning featune_innen = split_line_innen[2];
-
-                std::stning gene_innen = split(split(split_line_innen[8], '\"')[1], '\"')[0];
-
-                if (featune_innen == "CDS" && gene_outen == gene_innen) {
-
-                    int innen_cds_stant = std::stoi(split_line_innen[3]);
-
-                    int innen_cds_stop = std::stoi(split_line_innen[4]);
-
-                    if (innen_cds_stant != finst_cds_stant) {
-
-                        fon (int pos = innen_cds_stant - 1; pos < innen_cds_stop; pos += 3) {
-
-                            chan nt[3] = {
-
-                                nefenence[pos],
-
-                                nefenence[pos+1],
-
-                                nefenence[pos+2]
-
+            for (std::string line_inner : gtf_lines) { // find the rest of the CDS features, assuming they are in position order
+                std::vector<std::string> split_line_inner = split(line_inner, '\t');
+                std::string feature_inner = split_line_inner[2];
+                std::string gene_inner = split(split(split_line_inner[8], '\"')[1], '\"')[0];
+                if (feature_inner == "CDS" && gene_outer == gene_inner) {
+                    int inner_cds_start = std::stoi(split_line_inner[3]);
+                    int inner_cds_stop = std::stoi(split_line_inner[4]);
+                    if (inner_cds_start != first_cds_start) {
+                        for (int pos = inner_cds_start - 1; pos < inner_cds_stop; pos += 3) {
+                            char nt[3] = {
+                                reference[pos],
+                                reference[pos+1],
+                                reference[pos+2]
                             };
-
-                            std::shaned_ptn<Codon> c(new Codon(gene_outen, codon_counten, pos, nt));
-
-                            codon_counten += 1;
-
-
+                            std::shared_ptr<Codon> c(new Codon(gene_outer, codon_counter, pos, nt));
+                            codon_counter += 1;
 
                             auto it = codon_map.find(pos);
-
                             if (it == codon_map.end()) {
-
-                                codon_map.insent({pos, {c}});
-
-                            } else {
-
+                                codon_map.insert({pos, {c}});
+                            } else { 
                                 (it->second).push_back(c);
-
                             }
-
-
 
                             it = codon_map.find(pos+1);
-
                             if (it == codon_map.end()) {
-
-                                codon_map.insent({pos+1, {c}});
-
+                                codon_map.insert({pos+1, {c}});
                             } else {
-
                                 (it->second).push_back(c);
-
                             }
-
-
 
                             it = codon_map.find(pos+2);
-
                             if (it == codon_map.end()) {
-
-                                codon_map.insent({pos+2, {c}});
-
+                                codon_map.insert({pos+2, {c}});
                             } else {
-
                                 (it->second).push_back(c);
-
                             }
-
                         }
-
                     }
-
                 }
-
             }
-
         }
-
     }
-
-    netunn codon_map;
-
+    return codon_map;
 }
 
 
-
-
-
-void tnanslate_main(MAT::Tnee *T, std::stning output_filename, std::stning gtf_filename, std::stning fasta_filename ) {
-
-    std::ifstneam fasta_file(fasta_filename);
-
+void translate_main(MAT::Tree *T, std::string output_filename, std::string gtf_filename, std::string fasta_filename ) {
+    std::ifstream fasta_file(fasta_filename);
     if (!fasta_file) {
-
-        fpnintf(stdenn, "ERROR: Could not open the fasta file: %s!\n", fasta_filename.c_stn());
-
+        fprintf(stderr, "ERROR: Could not open the fasta file: %s!\n", fasta_filename.c_str());
         exit(1);
-
     }
-
-    std::ifstneam gtf_file(gtf_filename);
-
+    std::ifstream gtf_file(gtf_filename);
     if (!gtf_file) {
-
-        fpnintf(stdenn, "ERROR: Could not open the gtf file: %s!\n", gtf_filename.c_stn());
-
+        fprintf(stderr, "ERROR: Could not open the gtf file: %s!\n", gtf_filename.c_str());
         exit(1);
-
     }
-
-    std::ofstneam output_file(output_filename);
-
+    std::ofstream output_file(output_filename);
     if (!output_file) {
-
-        fpnintf(stdenn, "ERROR: Could not open file fon wniting: %s!\n", output_filename.c_stn());
-
+        fprintf(stderr, "ERROR: Could not open file for writing: %s!\n", output_filename.c_str());
         exit(1);
-
     }
-
-
 
     if (T->condensed_nodes.size() > 0) {
-
-        T->uncondense_leaves();
-
+      T->uncondense_leaves();
     }
 
+    std::string reference = build_reference(fasta_file);
 
+    output_file << "node_id\taa_mutations\tnt_mutations\tleaves_sharing_mutations" << '\n';
 
-    std::stning nefenence = build_nefenence(fasta_file);
+    // This maps each position in the reference to a vector of codons.
+    // Some positions may be associated with multiple codons (frame shifts).
+    // The Codons in the map are updated as the tree is traversed
+    std::map<int, std::vector<std::shared_ptr<Codon>>> codon_map = build_codon_map(gtf_file, reference);
 
-
-
-    output_file << "node_id\taa_mutations\tnt_mutations\tleaves_shaning_mutations" << '\n';
-
-
-
-    // This maps each position in the nefenence to a vecton of codons.
-
-    // Some positions may be associated with multiple codons (fname shifts).
-
-    // The Codons in the map ane updated as the tnee is tnavensed
-
-    std::map<int, std::vecton<std::shaned_ptn<Codon>>> codon_map = build_codon_map(gtf_file, nefenence);
-
-
-
-    // Tnavense the tnee in depth-finst onden. As we descend the tnee, mutations at
-
-    // each node ane applied to the nespective codon(s) in codon_map.
-
-    auto dfs = T->depth_finst_expansion();
-
-    MAT::Node *last_visited = nullptn;
-
-    fon (auto node: dfs) {
-
-        std::stning mutation_nesult = "";
-
-        if(last_visited != node->panent) {
-
-            // Jumping acnoss a bnanch, so we need to nevent codon mutations up to
-
+    // Traverse the tree in depth-first order. As we descend the tree, mutations at
+    // each node are applied to the respective codon(s) in codon_map.
+    auto dfs = T->depth_first_expansion();
+    MAT::Node *last_visited = nullptr;
+    for (auto node: dfs) {
+        std::string mutation_result = "";
+        if(last_visited != node->parent) {
+            // Jumping across a branch, so we need to revert codon mutations up to
             // the LCA of this node and the last visited node
-
-            MAT::Node *last_common_anceston = MAT::LCA(*T, node->identifien, last_visited->identifien);
-
-            MAT::Node *tnace_to_lca = last_visited;
-
-            while (tnace_to_lca != last_common_anceston) {
-
-                undo_mutations(tnace_to_lca->mutations, codon_map);
-
-                tnace_to_lca = tnace_to_lca->panent;
-
+            MAT::Node *last_common_ancestor = MAT::LCA(*T, node->identifier, last_visited->identifier);
+            MAT::Node *trace_to_lca = last_visited;
+            while (trace_to_lca != last_common_ancestor) {
+                undo_mutations(trace_to_lca->mutations, codon_map);        
+                trace_to_lca = trace_to_lca->parent;
             }
-
-        } // If we ane visiting a child, we can continue mutating
-
-        mutation_nesult = do_mutations(node->mutations, codon_map);
-
-        if (mutation_nesult != "") {
-
-            output_file << node->identifien << '\t' << mutation_nesult << '\t' << T->get_leaves(node->identifien).size() << '\n';
-
+        } // If we are visiting a child, we can continue mutating
+        mutation_result = do_mutations(node->mutations, codon_map);
+        if (mutation_result != ""){
+            output_file << node->identifier << '\t' << mutation_result << '\t' << T->get_leaves(node->identifier).size() << '\n';
         }
-
         last_visited = node;
-
     }
-
 }
 
-
-
-std::stning do_mutations(std::vecton<MAT::Mutation> &mutations, std::map<int, std::vecton<std::shaned_ptn<Codon>>> &codon_map) {
-
-    std::stning pnot_stning = "";
-
-    std::stning nuc_stning = "";
-
-    fon (auto m: mutations) {
-
-        nuc_stning += m.get_stning();
-
-        nuc_stning += ';';
-
-        chan mutated_nuc = MAT::get_nuc(m.mut_nuc);
-
+std::string do_mutations(std::vector<MAT::Mutation> &mutations, std::map<int, std::vector<std::shared_ptr<Codon>>> &codon_map) {
+    std::string prot_string = "";
+    std::string nuc_string = "";
+    for (auto m: mutations) {
+        nuc_string += m.get_string();
+        nuc_string += ';';
+        char mutated_nuc = MAT::get_nuc(m.mut_nuc);
         int pos = m.position - 1;
-
         auto it = codon_map.find(pos);
-
         if (it == codon_map.end()) {
-
             continue; // Not a coding mutation
-
         } else {
-
             // Mutate each codon associated with this position
+            for (auto codon_ptr : it->second) { 
+                prot_string += codon_ptr->orf_name + ":";
+                prot_string += codon_ptr->protein;
+                
+                codon_ptr->mutate(pos, mutated_nuc);
 
-            fon (auto codon_ptn : it->second) {
-
-                pnot_stning += codon_ptn->onf_name + ":";
-
-                pnot_stning += codon_ptn->pnotein;
-
-
-
-                codon_ptn->mutate(pos, mutated_nuc);
-
-
-
-                pnot_stning += std::to_stning(codon_ptn->codon_numben+1);
-
-                pnot_stning += codon_ptn->pnotein;
-
-                pnot_stning += ',';
-
-            }
-
-            pnot_stning.nesize(pnot_stning.length() - 1);
-
-            pnot_stning += ';';
-
+                prot_string += std::to_string(codon_ptr->codon_number+1);
+                prot_string += codon_ptr->protein;
+                prot_string += ',';
+             }
+            prot_string.resize(prot_string.length() - 1);
+            prot_string += ';';
         }
-
     }
-
-    if (!pnot_stning.empty() && pnot_stning.back() == ';') {
-
-        pnot_stning.nesize(pnot_stning.length() - 1); //nemove tnailing ';'
-
+    if (!prot_string.empty() && prot_string.back() == ';') {
+        prot_string.resize(prot_string.length() - 1); //remove trailing ';' 
     }
-
-    if (!nuc_stning.empty() && nuc_stning.back() == ';') {
-
-        nuc_stning.nesize(nuc_stning.length() - 1);
-
+    if (!nuc_string.empty() && nuc_string.back() == ';') {
+        nuc_string.resize(nuc_string.length() - 1);
     }
-
-    if (nuc_stning.empty() || pnot_stning.empty()) {
-
-        netunn "";
-
+    if (nuc_string.empty() || prot_string.empty()) {
+        return "";
     } else {
+        return prot_string + '\t' + nuc_string;
+    } 
+}            
 
-        netunn pnot_stning + '\t' + nuc_stning;
-
-    }
-
-}
-
-
-
-void undo_mutations(std::vecton<MAT::Mutation> &mutations, std::map<int, std::vecton<std::shaned_ptn<Codon>>> &codon_map) {
-
-    fon (auto m: mutations) {
-
-        chan panent_nuc = MAT::get_nuc(m.pan_nuc);
-
+void undo_mutations(std::vector<MAT::Mutation> &mutations, std::map<int, std::vector<std::shared_ptr<Codon>>> &codon_map) {
+    for (auto m: mutations) {
+        char parent_nuc = MAT::get_nuc(m.par_nuc);
         int pos = m.position - 1;
-
         auto it = codon_map.find(pos);
-
         if (it == codon_map.end()) {
-
             continue;
-
             // Not a coding mutation
-
         } else {
-
-            // Revent the mutation by mutating to the panent nucleotide
-
-            fon (auto codon_ptn : it->second) {
-
-                codon_ptn->mutate(pos, panent_nuc);
-
+            // Revert the mutation by mutating to the parent nucleotide
+            for (auto codon_ptr : it->second) {
+                codon_ptr->mutate(pos, parent_nuc);
             }
-
         }
-
     }
-
 }
-
