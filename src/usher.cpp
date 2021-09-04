6e059a12f53a432d907760eb29ea8033d7888e73
Yatish Turakhia <yatisht@stanford.edu>
2021-09-04 11:20:35 -0700

    Warning for -E moved outside the nested loop.

--- src/usher.cpp
+++ src/usher.cpp
@@ -810,13 +810,15 @@ int main(int argc, char** argv) {
                         }
                         if (num_best > max_uncertainty) {
                             fprintf(stderr, "WARNING: Number of parsimony-optimal placements exceeds maximum allowed value (%u). Ignoring sample %s.\n", max_uncertainty, sample.c_str());
-                        } else if (best_set_difference > max_parsimony) {
-                            fprintf(stderr, "WARNING: Parsimony score of the most parsimonious placement exceeds the maximum allowed value (%u). Ignoring sample %s.\n", max_parsimony, sample.c_str());
-                        } else {
+                        }  else if (best_set_difference <= max_parsimony) {
                             fprintf(stderr, "WARNING: Multiple parsimony-optimal placements found. Placement done without high confidence.\n");
                         }
+                    } 
+                    
+                    if (best_set_difference > max_parsimony) {
+                        fprintf(stderr, "WARNING: Parsimony score of the most parsimonious placement exceeds the maximum allowed value (%u). Ignoring sample %s.\n", max_parsimony, sample.c_str());
                     }
-                } else {
+                }  else {
                     fprintf(stderr, "Missing sample: %s\t Best parsimony score: %d\tNumber of parsimony-optimal placements: %zu\n", sample.c_str(), \
                             best_set_difference, num_best);
 
