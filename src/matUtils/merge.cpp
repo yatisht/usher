17f27a6ccb11c53073a67ca34c335896a42338cc
dtorvi <83394090+dtorvi@users.noreply.github.com>
2021-09-13 10:35:12 -0700

    Update merge.cpp and merge.hpp
    Removed some print statements and used mapper2_body.
    
    Removed extra merge file.

--- src/matUtils/merge.cpp
+++ src/matUtils/merge.cpp
@@ -2,7 +2,7 @@
 //uncondense leaves
 #include "merge.hpp"
 concurMap consistNodes;
-tbb::reader_writer_lock rw_lock;
+//tbb::reader_writer_lock rd_wr_lock;
 po::variables_map parse_merge_command(po::parsed_options parsed)
 {
     uint32_t num_cores = tbb::task_scheduler_init::default_num_threads();
@@ -46,18 +46,14 @@ bool consistent(MAT::Tree A, MAT::Tree B)
 
     //vectors of all leaves in both input trees
     std::vector<std::string> A_leaves = A.get_leaves_ids();
-    std::cout<<A_leaves.size()<<std::endl;
     std::vector<std::string> B_leaves = B.get_leaves_ids();
-        std::cout<<B_leaves.size()<<std::endl;
 
     //creates a vector of common_leaves between two input trees
     std::vector<std::string> common_leaves;
 
     set_intersection(B_leaves.begin(), B_leaves.end(), A_leaves.begin(), A_leaves.end(), std::back_inserter(common_leaves));
-    std::cout << common_leaves.size() << std::endl;
     if (common_leaves.size() == 0)
     {
-        std::cout << common_leaves.size() << std::endl;
         return true;
     }
     //creates two subtrees using the common_leaves
@@ -201,6 +197,7 @@ void merge_main(po::parsed_options parsed)
         fprintf(stderr, "ERROR: MAT files are not consistent!\n");
         exit(1);
     }
+    
     //Makes a copy of the baseMat files to add the samples to later
     MAT::Tree finalMat = get_tree_copy(baseMat);
     std::vector<std::string> samples;
@@ -215,7 +212,7 @@ void merge_main(po::parsed_options parsed)
 
     //concurMap::accessor ac;
     static tbb::affinity_partitioner ap;
-    std::cout << samples.size() << std::endl;
+    std::cout << "Number of samples to merge: " << samples.size() << std::endl;
 
     tbb::mutex m;
     //parallel loop that concurrently parses through all the new samples
@@ -261,7 +258,7 @@ void merge_main(po::parsed_options parsed)
                 //Roots bfs at closest consistent node identified in previous loop
                 //Restricts tree search to a smaller subtree
                 auto bfs = finalMat.breadth_first_expansion(curr);
-                placement_input inp;
+                mapper2_input inp;
                 //std::cout<<bfs[0]->mutations.size()<<std::endl;
 
                 std::vector<std::vector<MAT::Mutation> > node_excess_mutations(bfs.size());
@@ -292,7 +289,7 @@ void merge_main(po::parsed_options parsed)
                     inp.has_unique = &best_node_has_unique;
                     inp.best_j_vec = &best_j_vec;
                     inp.node_has_unique = &(node_has_unique);
-                    placement(inp, false, true);
+                    mapper2_body(inp, false, true);
                 }
 
                 if (finalMat.get_node(x) == NULL)
@@ -461,447 +458,16 @@ void merge_main(po::parsed_options parsed)
                         fprintf(stderr, "\n");
                     }
                 }
-                m.lock();
+               /** m.lock();
                 i++;
-                std::cout << i << ": " << finalMat.get_node(x)->parent->identifier << std::endl;
-                m.unlock();
-                /**
-                else
-                {
-                    min = anc.size() - 1;
-                }
-                m.lock();
-                finalMat.create_node(x, curr, -1);
-                MAT::Node *add = finalMat.get_node(x);
-                m.unlock();
-                for (int i = min; i >= 0; i--)
-                {
-                    for (auto m : ancestors[i]->mutations)
-                    {
-                        add->add_mutation(m);
-                    }
-                }**/
+                std::cout << i << ": " << finalMat.get_node(x)->identifier << std::endl;
+                m.unlock();**/
             }
         },
         ap);
-    std::cout << baseMat.get_num_leaves() << std::endl;
-    std::cout << otherMat.get_num_leaves() << std::endl;
-    std::cout << finalMat.get_num_leaves() << std::endl;
-
     //Save final MAT file
     finalMat.condense_leaves();
 
     MAT::save_mutation_annotated_tree(finalMat, output_filename);
     fprintf(stderr, "Completed in %ld msec \n\n", timer.Stop());
-}
-void placement(placement_input &input, bool compute_parsimony_scores, bool compute_vecs)
-{
-    //    TIMEIT();
-
-    // Variable to store the number of parsimony-increasing mutations to
-    // place sample at the current node
-    int set_difference = 0;
-
-    // Current smallest value of the number of parsimony-increasing mutations
-    // during the parallel search to place the same at some node in the tree
-    int best_set_difference = *input.best_set_difference;
-
-    std::vector<int> anc_positions;
-    std::vector<MAT::Mutation> ancestral_mutations;
-
-    // if node has some unique mutations not in new sample, placement should be
-    // done as a sibling
-    bool has_unique = false;
-    int node_num_mut = 0;
-    int num_common_mut = 0;
-
-    // For non-root nodes, add mutations common to current node (branch) to
-    // excess mutations. Set has_unique to true if a mutation unique to current
-    // node not in new sample is found.
-
-    if (!input.node->is_root())
-    {
-        size_t start_index = 0;
-        for (auto m1 : input.node->mutations)
-        {
-            node_num_mut++;
-            auto anc_nuc = m1.mut_nuc;
-            // if mutation is masked, treat it as a unique mutation (add as
-            // sibling)
-            if (m1.is_masked())
-            {
-                has_unique = true;
-                break;
-            }
-            assert(((anc_nuc - 1) & anc_nuc) == 0);
-            bool found = false;
-            bool found_pos = false;
-            for (size_t k = start_index; k < input.missing_sample_mutations->size(); k++)
-            {
-                auto m2 = (*input.missing_sample_mutations)[k];
-                start_index = k;
-                if (m1.position == m2.position)
-                {
-                    found_pos = true;
-                    if (m2.is_missing)
-                    {
-                        found = true;
-                        num_common_mut++;
-                    }
-                    else
-                    {
-                        auto nuc = m2.mut_nuc;
-                        if ((nuc & anc_nuc) != 0)
-                        {
-                            MAT::Mutation m;
-                            m.chrom = m1.chrom;
-                            m.position = m1.position;
-                            m.ref_nuc = m1.ref_nuc;
-                            m.par_nuc = m1.par_nuc;
-                            m.mut_nuc = anc_nuc;
-
-                            ancestral_mutations.emplace_back(m);
-                            anc_positions.emplace_back(m.position);
-                            assert((m.mut_nuc & (m.mut_nuc - 1)) == 0);
-                            if (compute_vecs)
-                            {
-                                (*input.excess_mutations).emplace_back(m);
-                            }
-
-                            // Ambiguous base
-                            //if ((nuc & (nuc-1)) != 0) {
-                            //    (*input.imputed_mutations).emplace_back(m);
-                            //}
-                            found = true;
-                            num_common_mut++;
-                            break;
-                        }
-                    }
-                }
-                if (m1.position < m2.position)
-                {
-                    break;
-                }
-            }
-            if (!found)
-            {
-                if (!found_pos && (anc_nuc == m1.ref_nuc))
-                {
-                    MAT::Mutation m;
-                    m.position = m1.position;
-                    m.chrom = m1.chrom;
-                    m.ref_nuc = m1.ref_nuc;
-                    m.par_nuc = m1.par_nuc;
-                    m.mut_nuc = anc_nuc;
-
-                    ancestral_mutations.emplace_back(m);
-                    anc_positions.emplace_back(m.position);
-                    assert((m.mut_nuc & (m.mut_nuc - 1)) == 0);
-                    if (compute_vecs)
-                    {
-                        (*input.excess_mutations).emplace_back(m);
-                    }
-
-                    num_common_mut++;
-                }
-                else
-                {
-                    has_unique = true;
-                }
-            }
-        }
-    }
-    else
-    {
-        for (auto m : input.node->mutations)
-        {
-            ancestral_mutations.emplace_back(m);
-            anc_positions.emplace_back(m.position);
-        }
-    }
-
-    // Add ancestral mutations to ancestral mutations. When multiple mutations
-    // at same position are found in the path leading from the root to the
-    // current node, add only the most recent mutation to the vector
-
-    {
-        auto n = input.node;
-        while (n->parent != NULL)
-        {
-            n = n->parent;
-            for (auto m : n->mutations)
-            {
-                if (!m.is_masked() && (std::find(anc_positions.begin(), anc_positions.end(), m.position) == anc_positions.end()))
-                {
-                    ancestral_mutations.emplace_back(m);
-                    anc_positions.emplace_back(m.position);
-                }
-            }
-        }
-    }
-
-    // sort by position. This helps speed up the search
-    std::sort(ancestral_mutations.begin(), ancestral_mutations.end());
-
-    // Iterate over missing sample mutations
-    for (auto m1 : (*input.missing_sample_mutations))
-    {
-        // Missing bases (Ns) are ignored
-        if (m1.is_missing)
-        {
-            continue;
-        }
-        size_t start_index = 0;
-        bool found_pos = false;
-        bool found = false;
-        bool has_ref = false;
-        auto anc_nuc = m1.ref_nuc;
-        if ((m1.mut_nuc & m1.ref_nuc) != 0)
-        {
-            has_ref = true;
-        }
-        // Check if mutation is found in ancestral_mutations
-        for (size_t k = start_index; k < ancestral_mutations.size(); k++)
-        {
-            auto m2 = ancestral_mutations[k];
-            // Masked mutations don't match anything
-            if (m2.is_masked())
-            {
-                continue;
-            }
-            start_index = k;
-            if (m1.position == m2.position)
-            {
-                found_pos = true;
-                anc_nuc = m2.mut_nuc;
-                if ((m1.mut_nuc & anc_nuc) != 0)
-                {
-                    found = true;
-                }
-                break;
-            }
-        }
-        if (found)
-        {
-            // If mutation is found in ancestral_mutations
-            // and if the missing sample base was ambiguous,
-            // add it to imputed_mutations
-            if (compute_vecs && ((m1.mut_nuc & (m1.mut_nuc - 1)) != 0))
-            {
-                MAT::Mutation m;
-                m.chrom = m1.chrom;
-                m.position = m1.position;
-                m.ref_nuc = m1.ref_nuc;
-                m.par_nuc = anc_nuc;
-                m.mut_nuc = anc_nuc;
-                input.imputed_mutations->emplace_back(m);
-            }
-        }
-        // If neither the same mutation nor another mutation at the same
-        // position is found in ancestor but if the missing sample can carry
-        // the reference allele, add a mutation with reference allele to
-        // imputed_mutations for the sample (it's not a parsimony-increasing
-        // mutation)
-        else if (!found_pos && has_ref)
-        {
-            if (compute_vecs && ((m1.mut_nuc & (m1.mut_nuc - 1)) != 0))
-            {
-                MAT::Mutation m;
-                m.chrom = m1.chrom;
-                m.position = m1.position;
-                m.ref_nuc = m1.ref_nuc;
-                m.par_nuc = anc_nuc;
-                m.mut_nuc = m1.ref_nuc;
-                input.imputed_mutations->emplace_back(m);
-            }
-        }
-        // In all other cases, it is a parsimony-increasing mutation. Return
-        // early if number of parsimony-increasing mutations exceeds the current
-        // best. Otherwise add the mutation to excess_mutations and to
-        // imputed_mutations, if base was originally ambiguous
-        else
-        {
-            MAT::Mutation m;
-            m.chrom = m1.chrom;
-            m.position = m1.position;
-            m.ref_nuc = m1.ref_nuc;
-            m.par_nuc = anc_nuc;
-            if (has_ref)
-            {
-                m.mut_nuc = m1.ref_nuc;
-            }
-            else
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    if (((1 << j) & m1.mut_nuc) != 0)
-                    {
-                        m.mut_nuc = (1 << j);
-                        ;
-                        break;
-                    }
-                }
-            }
-            assert((m.mut_nuc & (m.mut_nuc - 1)) == 0);
-            // If the missing sample base is ambiguous, add it to
-            // imputed_mutations
-            if (compute_vecs && ((m1.mut_nuc & (m1.mut_nuc - 1)) != 0))
-            {
-                input.imputed_mutations->emplace_back(m);
-            }
-            if (m.mut_nuc != m.par_nuc)
-            {
-                if (compute_vecs)
-                {
-                    input.excess_mutations->emplace_back(m);
-                }
-                set_difference += 1;
-                if (!compute_parsimony_scores && (set_difference > best_set_difference))
-                {
-                    return;
-                }
-            }
-        }
-    }
-
-    // For loop to add back-mutations for cases in which a mutation from the
-    // root to the current node consists of a non-reference allele but no such
-    // variant is found in the missing sample
-    for (auto m1 : ancestral_mutations)
-    {
-        size_t start_index = 0;
-        bool found = false;
-        bool found_pos = false;
-        auto anc_nuc = m1.mut_nuc;
-        for (size_t k = start_index; k < input.missing_sample_mutations->size(); k++)
-        {
-            // If ancestral mutation is masked, terminate the search for
-            // identical mutation
-            if (m1.is_masked())
-            {
-                break;
-            }
-            auto m2 = (*input.missing_sample_mutations)[k];
-            start_index = k;
-            if (m1.position == m2.position)
-            {
-                found_pos = true;
-                // Missing bases (Ns) are ignored
-                if (m2.is_missing)
-                {
-                    found = true;
-                    break;
-                }
-                if ((m2.mut_nuc & anc_nuc) != 0)
-                {
-                    found = true;
-                }
-            }
-        }
-        // If ancestral mutation is found, do nothing. Else, if last ancestor
-        // with mutation in the same position is found having reference allele, do
-        // nothing. If same position is found but not with the same allele, do
-        // nothing since the mutation must be added to excess mutations already
-        // in the previous loop. In all remaining cases, add the mutation to
-        // excess_mutations.
-        if (found)
-        {
-        }
-        else if (!found_pos && !m1.is_masked() && (anc_nuc == m1.ref_nuc))
-        {
-        }
-        else if (found_pos && !found)
-        {
-        }
-        else
-        {
-            MAT::Mutation m;
-            m.chrom = m1.chrom;
-            m.position = m1.position;
-            m.ref_nuc = m1.ref_nuc;
-            m.par_nuc = anc_nuc;
-            m.mut_nuc = m1.ref_nuc;
-            assert(m.is_masked() || ((m.mut_nuc & (m.mut_nuc - 1)) == 0));
-            if (m.mut_nuc != m.par_nuc)
-            {
-                set_difference += 1;
-                if (!compute_parsimony_scores && (set_difference > best_set_difference))
-                {
-                    return;
-                }
-                if (compute_vecs)
-                {
-                    (*input.excess_mutations).emplace_back(m);
-                }
-            }
-        }
-    }
-
-    // Set the number of parsimony-increasing mutations
-    if (compute_parsimony_scores)
-    {
-        *input.set_difference = set_difference;
-    }
-
-    // if sibling of internal node or leaf, ensure it is not equivalent to placing under parent
-    // if child of internal node, ensure all internal node mutations are present in the sample
-    if (input.node->is_root() || ((has_unique && !input.node->is_leaf() && (num_common_mut > 0) && (node_num_mut != num_common_mut)) ||
-                                  (input.node->is_leaf() && (num_common_mut > 0)) || (!has_unique && !input.node->is_leaf() && (node_num_mut == num_common_mut))))
-    {
-        rw_lock.lock_read();
-        if (set_difference > *input.best_set_difference)
-        {
-            rw_lock.unlock();
-            return;
-        }
-        rw_lock.unlock();
-
-        rw_lock.lock();
-        size_t num_leaves = input.T->get_num_leaves(input.node);
-        if (set_difference < *input.best_set_difference)
-        {
-            *input.best_set_difference = set_difference;
-            *input.best_node = input.node;
-            *input.best_node_num_leaves = num_leaves;
-            *input.best_j = input.j;
-            *input.num_best = 1;
-            *input.has_unique = has_unique;
-            *input.best_distance = input.distance;
-            (*input.node_has_unique)[input.j] = has_unique;
-            input.best_j_vec->clear();
-            input.best_j_vec->emplace_back(input.j);
-        }
-        else if (set_difference == *input.best_set_difference)
-        {
-            // Tie breaking strategy when multiple parsimony-optimal placements
-            // are found. it picks the node with a greater number of descendant
-            // leaves for placement. However, if the choice is between a parent
-            // and its child node, it picks the parent node if the number of
-            // descendant leaves of the parent that are not shared with the child
-            // node exceed the number of descendant leaves of the child.
-            if (((input.distance == *input.best_distance) &&
-                 ((num_leaves > *input.best_node_num_leaves) ||
-                  ((num_leaves == *input.best_node_num_leaves) && (*input.best_j < input.j)))) ||
-                (input.distance < *input.best_distance))
-            {
-                *input.best_set_difference = set_difference;
-                *input.best_node = input.node;
-                *input.best_node_num_leaves = num_leaves;
-                *input.best_j = input.j;
-                *input.has_unique = has_unique;
-                *input.best_distance = input.distance;
-            }
-            *input.num_best += 1;
-            (*input.node_has_unique)[input.j] = has_unique;
-            input.best_j_vec->emplace_back(input.j);
-        }
-        rw_lock.unlock();
-    }
-    else if (compute_parsimony_scores)
-    {
-        // Add 1 to the parsimony score for this node
-        // as its current best placement is equivalent
-        // to placing at parent/child
-        *input.set_difference = set_difference + 1;
-    }
 }
