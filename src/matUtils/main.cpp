92293123ed31143736d944932c619450f73f7a77
dtorvi <83394090+dtorvi@users.noreply.github.com>
2021-09-14 11:13:51 -0700

    Update main.cpp
    Added Merge Description

--- src/matUtils/main.cpp
+++ src/matUtils/main.cpp
@@ -19,7 +19,7 @@ int main (int argc, char** argv) {
     po::variables_map vm;
     po::parsed_options parsed = po::command_line_parser(argc, argv).options(global).positional(pos).allow_unregistered().run();
     //this help string shows up over and over, lets just define it once
-    std::string cnames[8] = {"COMMAND","summary","extract","annotate","uncertainty","mask","introduce","version"};
+    std::string cnames[8] = {"COMMAND","summary","extract","annotate","uncertainty","mask","introduce", "merge", "version"};
     std::string chelp[8] = {
         "DESCRIPTION\n\n",
         "calculates basic statistics and counts samples, mutations, and clades in the input MAT\n\n",
@@ -28,6 +28,7 @@ int main (int argc, char** argv) {
         "calculates sample placement uncertainty metrics and writes the results to tsv\n\n",
         "masks the input samples\n\n",
         "given sample region information, heuristically identifies points of geographic introduction along the phylogeny\n\n",
+        "merge all samples of two input MAT files into a single output MAT [EXPERIMENTAL]\n\n"
         "display version number\n\n"
     };
     try {
