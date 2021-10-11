26dcf0e7225d9221f0bbaa2d38c045c6039086d1
Yatish Turakhia <yturakhia@ucsd.edu>
2021-10-11 12:09:17 -0700

    Changed the method to test consistency.

--- src/matUtils/merge.hpp
+++ src/matUtils/merge.hpp
@@ -5,6 +5,6 @@ typedef tbb::concurrent_unordered_map<std::string, std::string> concurMap;
 
 po::variables_map parse_summary_command(po::parsed_options parsed);
 bool consistent(MAT::Tree T, MAT::Tree B, concurMap& consistNodes);
-bool chelper(MAT::Node* a, MAT::Node* b, concurMap& consistNodes);
+std::string get_first_leaf(MAT::Node* n);
 void merge_main(po::parsed_options parsed);
 MAT::Tree subtree(MAT::Tree tree, std::vector<std::string> common);
