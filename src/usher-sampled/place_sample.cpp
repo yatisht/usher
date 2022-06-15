d28000a906ed2cf4520f780139ce187769ede500
cheng ye <chye@ucsd.edu>
2022-06-15 15:52:08 -0700

    fix placement_stats

--- src/usher-sampled/place_sample.cpp
+++ src/usher-sampled/place_sample.cpp
@@ -362,6 +362,9 @@ struct Print_Thread{
         }
         if (!dry_run) {
             if (filter_placement(in, low_confidence_samples, num_epps, sample_name, max_uncertainty, max_parsimony)) {
+		if(placement_stats_file){
+			fputc('\n',placement_stats_file);
+		}
                 return;
             }
         }
