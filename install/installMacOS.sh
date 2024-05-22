dc696d6b4254554d6ecb9535fa2e9d81ea5a6f81
Yatish Turakhia <yatishturakhia@gmail.com>
2024-05-21 21:23:06 -0700

    Update installMacOS.sh

--- install/installMacOS.sh
+++ install/installMacOS.sh
@@ -1,4 +1,4 @@
-brew install cmake boost protobuf wget rsync openmpi libtool automake autoconf nasm gcc
+brew install cmake boost protobuf wget rsync openmpi libtool automake autoconf nasm
 wget https://github.com/intel/isa-l/archive/refs/tags/v2.30.0.tar.gz
 tar -xvf v2.30.0.tar.gz
 pushd isa-l-2.30.0
