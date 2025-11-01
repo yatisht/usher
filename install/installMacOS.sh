03446a083228c4ed6c1287c57d75a0ac58084104
Yatish Turakhia <yatishturakhia@gmail.com>
2025-10-31 19:58:29 -0700

    fixed macos install 8. modified workflow and installation script.

--- install/installMacOS.sh
+++ install/installMacOS.sh
@@ -2,17 +2,18 @@
 
 # Check if conda is available
 if ! command -v conda >/dev/null 2>&1; then
-    echo "ERROR: Conda is not installed or not found in your PATH." >&2
-    echo "Please install Miniconda or Anaconda before proceeding." >&2
-    exit 1
+    echo "WARNING: Installing conda since it is not installed or not found in your PATH." >&2
+    curl -fsSL https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -o miniconda.sh  
+    bash miniconda.sh -b -p "$HOME/miniconda" 
+    rm miniconda.sh 
+    export PATH="$HOME/miniconda/bin:$PATH" 
+    source "$HOME/miniconda/etc/profile.d/conda.sh"
 fi
 
 # create build directory
 startDir=$pwd
 cd $(dirname "$0")
 conda env create -f environment.yml -n usher -y
-conda init
-source ~/".$(basename $SHELL)rc"
 conda activate usher
 cd ..
 
