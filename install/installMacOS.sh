290a2c6b7596132cb7de22da1cbd4a8034bb4cda
Yatish Turakhia <yatishturakhia@gmail.com>
2025-10-31 20:21:58 -0700

    use latest macos for workflow. add matOptimize and mafft help.

--- install/installMacOS.sh
+++ install/installMacOS.sh
@@ -27,12 +27,12 @@ chmod +x faToVcf
 
 # install mafft
 if ! command -v mafft &> /dev/null; then 
-wget https://mafft.cbrc.jp/alignment/software/mafft-7.471-mac.zip
-unzip mafft-7.471-mac.zip
-cd mafft-mac/
-mv mafft.bat /usr/local/bin/mafft; mv mafftdir /usr/local/bin/
-cd ..
-rm -rf mafft-mac
+    wget https://mafft.cbrc.jp/alignment/software/mafft-7.471-mac.zip
+    unzip mafft-7.471-mac.zip
+    cd mafft-mac/
+    mv mafft.bat /usr/local/bin/mafft; mv mafftdir /usr/local/bin/
+    cd ..
+    rm -rf mafft-mac
 fi
 
 cd $startDir
